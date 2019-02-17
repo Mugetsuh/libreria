c7e50d9f1ac53d8fec5204959a32b9a5b682cfe5
German <shiniga94mi@gmail.com>
2019-02-17 15:43:03 -0500

    añadir atributos y metodos de libro

--- src/main/java/com/mycompany/librerialineaii/Libro.java
+++ src/main/java/com/mycompany/librerialineaii/Libro.java
@@ -11,4 +11,74 @@ package com.mycompany.librerialineaii;
  */
 public class Libro {
     
+    private char nombre;
+    
+    private char autor;
+    
+    private char editoria;
+    
+    private char genero;
+    
+    private float version;
+    
+    private long valorLibro;
+
+    public char getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(char nombre) {
+        this.nombre = nombre;
+    }
+
+    public char getAutor() {
+        return autor;
+    }
+
+    public void setAutor(char autor) {
+        this.autor = autor;
+    }
+
+    public char getEditoria() {
+        return editoria;
+    }
+
+    public void setEditoria(char editoria) {
+        this.editoria = editoria;
+    }
+
+    public char getGenero() {
+        return genero;
+    }
+
+    public void setGenero(char genero) {
+        this.genero = genero;
+    }
+
+    public float getVersion() {
+        return version;
+    }
+
+    public void setVersion(float version) {
+        this.version = version;
+    }
+
+    public long getValorLibro() {
+        return valorLibro;
+    }
+
+    public void setValorLibro(long valorLibro) {
+        this.valorLibro = valorLibro;
+    }
+
+    public Libro(char nombre, char autor, char editoria, char genero, float version, long valorLibro) {
+        this.nombre = nombre;
+        this.autor = autor;
+        this.editoria = editoria;
+        this.genero = genero;
+        this.version = version;
+        this.valorLibro = valorLibro;
+    }   
+    
+    
 }
