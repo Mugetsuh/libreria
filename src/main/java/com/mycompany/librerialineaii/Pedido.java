90344ddb8a9803bb7c1541e3d654feda7f621290
German <shiniga94mi@gmail.com>
2019-02-17 15:36:56 -0500

    añadir atributos y metodos de pedido

--- src/main/java/com/mycompany/librerialineaii/Pedido.java
+++ src/main/java/com/mycompany/librerialineaii/Pedido.java
@@ -11,4 +11,51 @@ package com.mycompany.librerialineaii;
  */
 public class Pedido {
     
+    private int idPedido;
+    
+    private long fecha;
+    
+    private int idCliente;
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public long getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(long fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+    
+    public Pedido(int idPedido, long fecha, int idCliente) {
+        this.idPedido = idPedido;
+        this.fecha = fecha;
+        this.idCliente = idCliente;
+    }
+
+    public void agragarLibro(){
+        
+    }
+    
+    public void quitarLibro(){
+        
+    }
+    
+    public void consultarLibro(){
+        
+    }
 }
